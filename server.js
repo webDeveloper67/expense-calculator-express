7c11da7e7df2dad8128282c77d1c4fb695100c6e
mdee <design4web2014@gmail.com>
2019-08-14 08:33:59 +0430

    prepare for deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -13,6 +14,16 @@ app.use(express.json({extended: false}));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/verify', require('./routes/verify'));
 app.use('/api/expenses', require('./routes/expenses'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+	// set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	);
+}
 
 const port = process.env.port || 5000;
 
